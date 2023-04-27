f012d20536aede8642852c8f5b6d8783e56c38b1
Kamil <mr.kaam@gmail.com>
2023-04-27 12:03:37 +0200

    TOPBAR&NAV: open nav in devices with width above 576px

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const nav = document.querySelector('.nav');
+const main = document.querySelector('.main');
 
 const toggleNav = () => {
 	nav.classList.toggle('nav--active');
 	burgerBtn.classList.toggle('burger-btn--active');
+	main.classList.toggle('main--active');
 };
 
 burgerBtn.addEventListener('click', toggleNav);
