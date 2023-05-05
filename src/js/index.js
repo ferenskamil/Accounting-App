a90c153e7dc04c68d107709d906672484e6092f1
Kamil <mr.kaam@gmail.com>
2023-05-05 15:00:04 +0200

    NAV: add shadow on tablet devices

--- src/js/index.js
+++ src/js/index.js
@@ -2,12 +2,14 @@ const burgerBtn = document.querySelector('.burger-btn');
 const nav = document.querySelector('.nav');
 const main = document.querySelector('.main');
 const topbar = document.querySelector('.topbar');
+const shadow = document.querySelector('.shadow');
 
 const toggleNav = () => {
 	nav.classList.toggle('nav--active');
 	burgerBtn.classList.toggle('burger-btn--active');
 	main.classList.toggle('main--active');
 	topbar.classList.toggle('topbar--active');
+	shadow.classList.toggle('shadow--active');
 };
 
 burgerBtn.addEventListener('click', toggleNav);
