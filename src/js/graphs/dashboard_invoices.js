7b39ca66621e26aea9bf78bacd16c78208cb9efb
Kamil <mr.kaam@gmail.com>
2023-05-07 09:11:24 +0200

    DASHBOARD: details
    - change class names to very relevant
    - add graph from graph.js
    - add rwd display on various devices
    - remove unecessary comments

---
+++ src/js/graphs/dashboard_invoices.js
@@ -0,0 +1,22 @@
+const invoicesSumChart = document.querySelector('#invoices-sum-chart');
+
+new Chart(invoicesSumChart, {
+	type: 'line',
+	data: {
+		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+		datasets: [
+			{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
+});
