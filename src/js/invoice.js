011d81b73716e095b52c2b900c7cb8d2545cc185
Kamil <mr.kaam@gmail.com>
2023-05-18 10:22:36 +0200

    INVOICE-EDIT FORM: form validation (js)
    Function:
    - marks inputs that are empty
    - prevents the form from being submitted (event.preventDefault() ) if it is not filled out correctly
    - returns true or false depending on whether the form has been filled in correctly
    - invoked by clicking on the button

--- src/js/invoice.js
+++ src/js/invoice.js
@@ -1,3 +1,5 @@
+// OPEN / HIDE EDIT FORM
+
 const invoiceEditBtn = document.querySelector('.invoice__settings-edit');
 const invoiceEditBackBtn = document.querySelector('.invoice__edit-back');
 const invoiceEditForm = document.querySelector('.invoice__edit');
@@ -18,3 +20,40 @@ const closeInvoiceEditForm = () => {
 
 invoiceEditBtn.addEventListener('click', openInvoiceEditForm);
 invoiceEditBackBtn.addEventListener('click', closeInvoiceEditForm);
+
+// =============================================
+// EDIT FORM VALIDATION
+const allInputs = document.querySelectorAll('.invoice__edit-form-box input');
+const submitBtnEditForm = document.querySelector(
+	'.invoice__edit-form-box-submit-btn button'
+);
+
+const validationInvoiceEditForm = (e) => {
+	let isOK = true;
+	allInputs.forEach((input) => {
+		input.classList.remove('invoice__edit-form-box-input--empty');
+
+		if (input.value === '') {
+			input.classList.add('invoice__edit-form-box-input--empty');
+			isOK = false;
+		}
+	});
+
+	// Block submit form if any input is empty
+	if (!isOK) e.preventDefault();
+
+	return isOK;
+};
+submitBtnEditForm.addEventListener('click', (e) =>
+validationInvoiceEditForm(e)
+);
+
+// ============================================================
+// NOTES
+// key shortcuts for testing
+// const keyShortcuts = (e) => {
+// 	if (e.key === 'Enter') {
+// 		alert('test');
+// 	}
+// };
+// document.addEventListener('keydown', (e) => keyShortcuts(e));
