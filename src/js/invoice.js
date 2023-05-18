377d7b8b69efcc55c54f056f85292c830a145db9
Kamil <mr.kaam@gmail.com>
2023-05-18 13:43:37 +0200

    INVOICE EDIT-FORM: transfer data from edit form to PREVIEW (js)
    Funkcja:
    - Transfers the values entered in the form to the invoice preview
    - Invoked when the "save" button is clicked or when the page is loaded
    
    - Zmiany id inputów i spanów (odpowiednio dodano końcówkę "-edit" lub "view")
    
    Other minor improvements include:
    - Changing the id of inputs and spans (adding the suffix "-edit" or "-view")
    - Converting "term" input to select
    - Adding a placeholder to the inputs with "NIP" number

--- src/js/invoice.js
+++ src/js/invoice.js
@@ -1,5 +1,4 @@
 // OPEN / HIDE EDIT FORM
-
 const invoiceEditBtn = document.querySelector('.invoice__settings-edit');
 const invoiceEditBackBtn = document.querySelector('.invoice__edit-back');
 const invoiceEditForm = document.querySelector('.invoice__edit');
@@ -29,6 +28,9 @@ const submitBtnEditForm = document.querySelector(
 );
 
 const validationInvoiceEditForm = (e) => {
+	// prevent default only for testing
+	e.preventDefault();
+
 	let isOK = true;
 	allInputs.forEach((input) => {
 		input.classList.remove('invoice__edit-form-box-input--empty');
@@ -50,11 +52,94 @@ submitBtnEditForm.addEventListener('click', (e) =>
 );
 
 // =============================================
+// TRANSFERRING DATA FROM EDIT FORM TO PREVIEW
+
+const transferDataFromEditFormToPreview = () => {
+	const invoiceNoEditForm = document.querySelector('#invoice-no-edit').value;
+	const dateEditForm = document.querySelector('#date-edit').value;
+	const cityEditForm = document.querySelector('#city-edit').value;
+	const bankEditForm = document.querySelector('#bank-edit').value;
+	const accountNoEditForm = document.querySelector('#account-no-edit').value;
+	const termEditForm = document.querySelector('#term-edit').value;
+
+	const sellerNameEditForm =
+		document.querySelector('#seller-name-edit').value;
+	const sellerAddress1EditForm = document.querySelector(
+		'#seller-address1-edit'
+	).value;
+	const sellerAddress2EditForm = document.querySelector(
+		'#seller-address2-edit'
+	).value;
+	const sellerNIPEditForm = document.querySelector('#seller-NIP-edit').value;
+
+	const customerNameEditForm = document.querySelector(
+		'#customer-name-edit'
+	).value;
+	const customerAddress1EditForm = document.querySelector(
+		'#customer-address1-edit'
+	).value;
+	const customerAddress2EditForm = document.querySelector(
+		'#customer-address2-edit'
+	).value;
+	const customerNIPEditForm =
+		document.querySelector('#customer-NIP-edit').value;
+
+	const commentTextAreaEditForm =
+		document.querySelector('#comment-edit').value;
+
+	let invoiceNoView = document.querySelector('#invoice-no-view');
+	let dateView = document.querySelector('#date-view');
+	let cityView = document.querySelector('#city-view');
+	let bankView = document.querySelector('#bank-view');
+	let accountNoView = document.querySelector('#account-no-view');
+	let termView = document.querySelector('#term-view');
+
+	let sellerNameView = document.querySelector('#seller-name-view');
+	let sellerAddress1View = document.querySelector('#seller-address1-view');
+	let sellerAddress2View = document.querySelector('#seller-address2-view');
+	let sellerNIPView = document.querySelector('#seller-NIP-view');
+
+	let customerNameView = document.querySelector('#customer-name-view');
+	let customerAddress1View = document.querySelector(
+		'#customer-address1-view'
+	);
+	let customerAddress2View = document.querySelector(
+		'#customer-address2-view'
+	);
+	let customerNIPView = document.querySelector('#customer-NIP-view');
+
+	const commentTextAreaView = document.querySelector('#comment-view');
+
+	invoiceNoView.textContent = invoiceNoEditForm;
+	dateView.textContent = dateEditForm;
+	cityView.textContent = cityEditForm;
+	bankView.textContent = bankEditForm;
+	accountNoView.textContent = accountNoEditForm;
+	termView.textContent = termEditForm;
+
+	sellerNameView.textContent = sellerNameEditForm;
+	sellerAddress1View.textContent = sellerAddress1EditForm;
+	sellerAddress2View.textContent = sellerAddress2EditForm;
+	sellerNIPView.textContent = sellerNIPEditForm;
+
+	customerNameView.textContent = customerNameEditForm;
+	customerAddress1View.textContent = customerAddress1EditForm;
+	customerAddress2View.textContent = customerAddress2EditForm;
+	customerNIPView.textContent = customerNIPEditForm;
+
+	commentTextAreaView.textContent = commentTextAreaEditForm;
+};
+
+submitBtnEditForm.addEventListener('click', transferDataFromEditFormToPreview);
+
+// =============================================
 // DISPLAY (OR NOT) EDIT FORM AFTER LOAD PAGE
 
 const editFormOnLoaded = (e) => {
 	if (!validationInvoiceEditForm(e)) {
 		openInvoiceEditForm();
+	} else {
+		transferDataFromEditFormToPreview();
 	}
 };
 
