813c026c30bbfb9f63f97c09b5cb6e0525395d43
Kamil <mr.kaam@gmail.com>
2023-05-25 00:11:13 +0200

    INVOICE EDIT-FORM: function that sanitizes the value in input (JS)

--- src/js/invoice.js
+++ src/js/invoice.js
@@ -146,6 +146,51 @@ const editFormOnLoaded = (e) => {
 document.addEventListener('DOMContentLoaded', (e) => editFormOnLoaded(e));
 
 // ============================================================
+// SANITIZE VALUE IN BANK ACCOUNT INPUT
+const accountNoInput = document.querySelector('#account-no-edit');
+const sanitizeAccountNoInput = (e) => {
+	// In the future I can to this with regex
+	let accountNoString = accountNoInput.value;
+	let workString;
+
+	if (!parseInt(e.data) && e.data !== '0') {
+		accountNoString = accountNoString.slice(0, -1);
+	}
+
+	switch (accountNoString.length) {
+		case 2:
+			workString = ''.concat(accountNoString.slice(0, 2), ' ');
+			break;
+		case 7:
+			workString = ''.concat(accountNoString.slice(0, 7), ' ');
+			break;
+		case 12:
+			workString = ''.concat(accountNoString.slice(0, 12), ' ');
+			break;
+		case 17:
+			workString = ''.concat(accountNoString.slice(0, 17), ' ');
+			break;
+		case 22:
+			workString = ''.concat(accountNoString.slice(0, 22), ' ');
+			break;
+		case 27:
+			workString = ''.concat(accountNoString.slice(0, 27), ' ');
+			break;
+		case 33:
+			accountNoString = accountNoString.slice(0, -1);
+			break;
+		default:
+			break;
+	}
+
+	if (workString) {
+		accountNoString = workString;
+	}
+	accountNoInput.value = accountNoString;
+};
+
+accountNoInput.addEventListener('input', (e) => sanitizeAccountNoInput(e));
+// ============================================================
 // NOTES
 // key shortcuts for testing
 // const keyShortcuts = (e) => {
