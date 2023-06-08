5171cb1bf126e94d3a4baa46b7289e4ba14eae19
Kamil <mr.kaam@gmail.com>
2023-06-08 18:10:16 +0200

    INVOICE: changes in "transfer_data_from_edit_to_preview.js" file
    Add "copyAndDisplay()" function:
    - This function takes the text from the element with the specified id and displays it in another indicated element
    
    Also:
    - Create an additional function "displayInvoiceInfoInPreview()" and invoke it in the function "transferDataFromEditFormToPreview()".
    - Code has been moved to the "displayInvoiceInfoInPreview()" function, which is responsible for displaying invoice information (e.g., date, contractor details) in the preview.
    - Changed the order of functions (moving the "displayInvoiceInfoInPreview()" function to the beginning of the document)

--- src/js/invoice/transfer_data_from_edit_to_preview.js
+++ src/js/invoice/transfer_data_from_edit_to_preview.js
@@ -28,6 +28,36 @@ class Service {
 	}
 }
 
+const transferDataFromEditFormToPreview = () => {
+	displayInvoiceInfoInPreview();
+	displayServicesInPreview();
+	displayInvoiceSummaryInPreview();
+};
+
+const copyAndDisplay = (idEditString, idPreviewString) => {
+	const fromWhere = document.querySelector(`${idEditString}`).value;
+	const toWhere = document.querySelector(`${idPreviewString}`);
+	toWhere.textContent = fromWhere;
+};
+
+const displayInvoiceInfoInPreview = () => {
+	copyAndDisplay('#invoice-no-edit', '#invoice-no-view');
+	copyAndDisplay('#date-edit', '#date-view');
+	copyAndDisplay('#city-edit', '#city-view');
+	copyAndDisplay('#bank-edit', '#bank-view');
+	copyAndDisplay('#account-no-edit', '#account-no-view');
+	copyAndDisplay('#term-edit', '#term-view');
+	copyAndDisplay('#seller-name-edit', '#seller-name-view');
+	copyAndDisplay('#seller-address1-edit', '#seller-address1-view');
+	copyAndDisplay('#seller-address2-edit', '#seller-address2-view');
+	copyAndDisplay('#seller-NIP-edit', '#seller-NIP-view');
+	copyAndDisplay('#customer-name-edit', '#customer-name-view');
+	copyAndDisplay('#customer-address1-edit', '#customer-address1-view');
+	copyAndDisplay('#customer-address2-edit', '#customer-address2-view');
+	copyAndDisplay('#customer-NIP-edit', '#customer-NIP-view');
+	copyAndDisplay('#comment-edit', '#comment-view');
+};
+
 const getServicesFromEditFormToArr = () => {
 	const servicesFromEditForm = document.querySelectorAll(
 		'.invoice__edit-form-box-services-table tbody tr'
@@ -129,85 +159,6 @@ const displayInvoiceSummaryInPreview = () => {
 	numericSpan.textContent = parseFloat(totalGrossEditForm.textContent);
 
 	verbalSpan.textContent = verbalNotation(numericSpan.textContent);
-};
-
-const transferDataFromEditFormToPreview = () => {
-	const invoiceNoEditForm = document.querySelector('#invoice-no-edit').value;
-	const dateEditForm = document.querySelector('#date-edit').value;
-	const cityEditForm = document.querySelector('#city-edit').value;
-	const bankEditForm = document.querySelector('#bank-edit').value;
-	const accountNoEditForm = document.querySelector('#account-no-edit').value;
-	const termEditForm = document.querySelector('#term-edit').value;
-
-	const sellerNameEditForm =
-		document.querySelector('#seller-name-edit').value;
-	const sellerAddress1EditForm = document.querySelector(
-		'#seller-address1-edit'
-	).value;
-	const sellerAddress2EditForm = document.querySelector(
-		'#seller-address2-edit'
-	).value;
-	const sellerNIPEditForm = document.querySelector('#seller-NIP-edit').value;
-
-	const customerNameEditForm = document.querySelector(
-		'#customer-name-edit'
-	).value;
-	const customerAddress1EditForm = document.querySelector(
-		'#customer-address1-edit'
-	).value;
-	const customerAddress2EditForm = document.querySelector(
-		'#customer-address2-edit'
-	).value;
-	const customerNIPEditForm =
-		document.querySelector('#customer-NIP-edit').value;
-
-	const commentTextAreaEditForm =
-		document.querySelector('#comment-edit').value;
-
-	let invoiceNoView = document.querySelector('#invoice-no-view');
-	let dateView = document.querySelector('#date-view');
-	let cityView = document.querySelector('#city-view');
-	let bankView = document.querySelector('#bank-view');
-	let accountNoView = document.querySelector('#account-no-view');
-	let termView = document.querySelector('#term-view');
-
-	let sellerNameView = document.querySelector('#seller-name-view');
-	let sellerAddress1View = document.querySelector('#seller-address1-view');
-	let sellerAddress2View = document.querySelector('#seller-address2-view');
-	let sellerNIPView = document.querySelector('#seller-NIP-view');
-
-	let customerNameView = document.querySelector('#customer-name-view');
-	let customerAddress1View = document.querySelector(
-		'#customer-address1-view'
-	);
-	let customerAddress2View = document.querySelector(
-		'#customer-address2-view'
-	);
-	let customerNIPView = document.querySelector('#customer-NIP-view');
-
-	const commentTextAreaView = document.querySelector('#comment-view');
-
-	invoiceNoView.textContent = invoiceNoEditForm;
-	dateView.textContent = dateEditForm;
-	cityView.textContent = cityEditForm;
-	bankView.textContent = bankEditForm;
-	accountNoView.textContent = accountNoEditForm;
-	termView.textContent = termEditForm;
-
-	sellerNameView.textContent = sellerNameEditForm;
-	sellerAddress1View.textContent = sellerAddress1EditForm;
-	sellerAddress2View.textContent = sellerAddress2EditForm;
-	sellerNIPView.textContent = sellerNIPEditForm;
-
-	customerNameView.textContent = customerNameEditForm;
-	customerAddress1View.textContent = customerAddress1EditForm;
-	customerAddress2View.textContent = customerAddress2EditForm;
-	customerNIPView.textContent = customerNIPEditForm;
-
-	commentTextAreaView.textContent = commentTextAreaEditForm;
-
-	displayServicesInPreview();
-	displayInvoiceSummaryInPreview();
 };
 
 submitBtn.addEventListener('click', transferDataFromEditFormToPreview);
