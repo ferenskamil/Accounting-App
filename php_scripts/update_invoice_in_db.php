747124dede67fbf6802a3cb2070c9a10cbd09762
Kamil <mr.kaam@gmail.com>
2023-07-18 16:06:48 +0200

    Fix: Save in database the missing customer information from edit form;

--- php_scripts/update_invoice_in_db.php
+++ php_scripts/update_invoice_in_db.php
@@ -65,12 +65,15 @@ if (in_array($_POST['invoice-no'], $invoice_nums)) {
         `sum_net`, `sum_gross`, `city`, `bank`, 
         `account_no`, `payment_term`, 
         `to_pay`, `to_pay_in_words`, 
-        `additional_notes`) 
-        VALUES ('17', :no , :date,
+        `additional_notes`, `customer_name`, `customer_address1`, 
+        `customer_address2`, `customer_company_no`) 
+        VALUES (:user_id, :no , :date,
         :sum_net, :sum_gross ,:city, :bank,
         :account_no , :term,
         :to_pay, :to_pay_in_words,
-        :comment)");
+        :comment, :customer_name, :customer_address1,
+        :customer_address2, :customer_company_no)");
+        $query->bindvalue(':user_id', $_SESSION['id'], PDO::PARAM_STR);
         $query->bindvalue(':no', $_POST['invoice-no'], PDO::PARAM_STR);
         $query->bindvalue(':date', $_POST['date'], PDO::PARAM_STR);
         $query->bindvalue(':sum_net', $_POST['total-net'], PDO::PARAM_STR);
@@ -82,6 +85,10 @@ if (in_array($_POST['invoice-no'], $invoice_nums)) {
         $query->bindvalue(':to_pay',$_POST['to-pay-numeric'], PDO::PARAM_STR);
         $query->bindvalue(':to_pay_in_words',$_POST['to-pay-verbal'], PDO::PARAM_STR);
         $query->bindvalue(':comment', $_POST['comment'], PDO::PARAM_STR);
+        $query->bindvalue(':customer_name', $_POST['customer-name'], PDO::PARAM_STR);
+        $query->bindvalue(':customer_address1', $_POST['customer-address1'], PDO::PARAM_STR);
+        $query->bindvalue(':customer_address2', $_POST['customer-address2'], PDO::PARAM_STR);
+        $query->bindvalue(':customer_company_no', $_POST['customer-company-no'], PDO::PARAM_STR);
         $query->execute();
         header('Location: ../invoice.php');
 
