30feffc9e9d2e5a7a10e95c0af60ed507261e8a7
Kamil <mr.kaam@gmail.com>
2023-07-24 12:41:29 +0200

    Feat: edit an invoice in the database

--- php_scripts/update_invoice_in_db.php
+++ php_scripts/update_invoice_in_db.php
@@ -58,9 +58,55 @@ foreach($db_invoices_nums as $num) {
         $invoice_nums[] = $num['no'];
 }
 
-if (in_array($_POST['invoice-no'], $invoice_nums)) {
-        header('Location: ./add_invoice_to_db_confirmation.php');
-} else {
+if (isset($_SESSION['is_user_wants_edit']) && $_SESSION['is_user_wants_edit'] === true) {
+        // kod edytujący konkretną fakturę 
+        $query = $db->prepare("UPDATE invoices
+                SET
+                        `date` = :date,
+                        sum_net = :sum_net,
+                        sum_gross = :sum_gross,
+                        city = :city,
+                        bank = :bank, 
+                        account_no = :account_no,
+                        payment_term = :term,
+                        to_pay = :to_pay_numeric,
+                        to_pay_in_words = :to_pay_verbal,
+                        additional_notes = :comment,
+                        seller_name = :seller_name,
+                        seller_address1 = :seller_address1,
+                        seller_address2 = :seller_address2,
+                        seller_company_no = :seller_company_no, 
+                        customer_name = :customer_name,
+                        customer_address1 = :customer_address1,
+                        customer_address2 = :customer_address2,
+                        customer_company_no = :customer_company_no
+                WHERE
+                        user_id = :id AND no = :invoice_no");
+        $query->bindvalue(':date', $_POST['date'], PDO::PARAM_STR);
+        $query->bindvalue(':sum_net', $_POST['total-net'], PDO::PARAM_STR);
+        $query->bindvalue(':sum_gross', $_POST['total-gross'], PDO::PARAM_STR);
+        $query->bindvalue(':city', $_POST['city'], PDO::PARAM_STR);
+        $query->bindvalue(':bank', $_POST['bank'], PDO::PARAM_STR);
+        $query->bindvalue(':account_no', $_POST['account-no'], PDO::PARAM_STR);
+        $query->bindvalue(':term', $_POST['term'], PDO::PARAM_STR);
+        $query->bindvalue(':to_pay_numeric',$_POST['to-pay-numeric'], PDO::PARAM_STR);
+        $query->bindvalue(':to_pay_verbal',$_POST['to-pay-verbal'], PDO::PARAM_STR);
+        $query->bindvalue(':comment', $_POST['comment'], PDO::PARAM_STR);
+        $query->bindvalue(':seller_name', $_POST['seller-name'], PDO::PARAM_STR);
+        $query->bindvalue(':seller_address1', $_POST['seller-address1'], PDO::PARAM_STR);
+        $query->bindvalue(':seller_address2', $_POST['seller-address2'], PDO::PARAM_STR);
+        $query->bindvalue(':seller_company_no', $_POST['seller-company-no'], PDO::PARAM_STR);
+        $query->bindvalue(':customer_name', $_POST['customer-name'], PDO::PARAM_STR);
+        $query->bindvalue(':customer_address1', $_POST['customer-address1'], PDO::PARAM_STR);
+        $query->bindvalue(':customer_address2', $_POST['customer-address2'], PDO::PARAM_STR);
+        $query->bindvalue(':customer_company_no', $_POST['customer-company-no'], PDO::PARAM_STR);
+        $query->bindvalue(':id', $_SESSION['id'], PDO::PARAM_INT);
+        $query->bindvalue(':invoice_no', $_POST['invoice-no'], PDO::PARAM_STR);
+        $query->execute();
+
+        $_SESSION['comment_after_edit'] = "ZMIENIONO FAKTURĘ TEST";
+        unset($_SESSION['is_user_wants_edit']);
+} elseif (!in_array($_POST['invoice-no'], $invoice_nums)) {
         $query = $db->prepare("INSERT INTO invoices (`user_id`, `no`, `date`, 
         `sum_net`, `sum_gross`, `city`, `bank`, 
         `account_no`, `payment_term`, 
@@ -97,9 +143,10 @@ if (in_array($_POST['invoice-no'], $invoice_nums)) {
         $query->bindvalue(':seller_address2', $_POST['seller-address2'], PDO::PARAM_STR);
         $query->bindvalue(':seller_company_no', $_POST['seller-company-no'], PDO::PARAM_STR);
         $query->execute();
-        header('Location: ../invoice_edit.php');
+
+        $_SESSION['comment_after_edit'] = "DODANO FAKTURĘ TEST";
+} else {
+        $_SESSION['comment_after_edit'] = "Coś poszło nie tak. Nie można dodać nr faktury, bo taki już istnieje. Nie można edytować obecnego nr bo użytkonik nie zgadza się na edycję.";
 }
-
-// unset $_SESSION['is_user_wants_edit'] after edit
-
+echo "successfull edit";
 ?>
