7b0dcdaa0cdddd426cc3e5815b598279ad53a7dd
Kamil <mr.kaam@gmail.com>
2023-08-01 09:33:58 +0200

    Feat: script to delete invoice and services from db

--- php_scripts/delete_invoice.php
+++ php_scripts/delete_invoice.php
@@ -1,4 +1,31 @@
 <?php
-echo "Hello World";
+session_start();
+
+$invoice_no_to_delete = $_POST['pop-up-invoice-no-hidden-input'];
+
+require_once 'db_database.php';
+
+// Find invoice id
+$db_invoice_id_query = $db->prepare("SELECT id FROM invoices
+        WHERE user_id = :user_id AND no = :invoice_no");
+$db_invoice_id_query->bindValue(':user_id', $_SESSION['id'], PDO::PARAM_STR);
+$db_invoice_id_query->bindValue(':invoice_no', $invoice_no_to_delete, PDO::PARAM_STR);
+$db_invoice_id_query->execute();
+$invoice_id = $db_invoice_id_query->fetch(PDO::FETCH_ASSOC);
+$invoice_id = $invoice_id['id'];
+
+// Delete invoice
+$db_delete_invoice_query = $db->prepare("DELETE FROM invoices
+        WHERE id = :id");
+$db_delete_invoice_query->bindValue(':id', $invoice_id, PDO::PARAM_INT);
+$db_delete_invoice_query->execute();
+
+// Delete services
+$db_delete_services_query = $db->prepare("DELETE FROM services
+        WHERE invoice_id = :invoice_id");
+$db_delete_services_query->bindValue(':invoice_id', $invoice_id, PDO::PARAM_INT);
+$db_delete_services_query->execute();
+
+header('Location: ../invoice_list.php');
 
 ?>
