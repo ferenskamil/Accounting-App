520383c77b99c0200d6722b8ad3af20396dc2883
Kamil <mr.kaam@gmail.com>
2023-08-09 13:53:20 +0200

    Refactor: change the order of the functions in file (no changes in code)

--- src/js/invoice/edit_form_services.js
+++ src/js/invoice/edit_form_services.js
@@ -49,6 +49,71 @@ const convertNumtoAmount = (num, currencyString) => {
 };
 
 // ============================================================
+// UPDATE DATA IN SERVICE TABLE
+const updateServicesTable = () => {
+	updateItemsNumbers();
+	showEmptyInfo();
+	calculateTableSummary();
+};
+
+const updateItemsNumbers = () => {
+	const itemsNoSpans = document.querySelectorAll(
+		'.invoice-edit__form-box-services-table .service-item-number'
+	);
+
+	const positionHiddenInputs = document.querySelectorAll(
+		'.position-hidden-input'
+	);
+
+	for (let i = 0; i < itemsNoSpans.length; i++) {
+		itemsNoSpans[i].textContent = i + 1;
+	}
+
+	for (let i = 0; i < positionHiddenInputs.length; i++) {
+		positionHiddenInputs[i].value = i + 1;
+	}
+};
+
+const showEmptyInfo = () => {
+	const services = document.querySelectorAll('#services-table tbody tr');
+	const emptyInfo = document.querySelector('#services-table tbody .empty-info');
+	emptyInfo.style.display = services.length === 1 ? 'grid' : 'none';
+};
+
+const calculateTableSummary = () => {
+	sumUpValues('.service-item-net-sum', '.invoice-total-net');
+	sumUpValues('.service-item-gross-sum', '.invoice-total-gross');
+};
+
+const sumUpValues = (inputsArrSelectorAll, outputSpanSelector) => {
+	const inputsArr = document.querySelectorAll(inputsArrSelectorAll);
+	const outputSpan = document.querySelector(outputSpanSelector);
+
+	let sum = 0;
+	inputsArr.forEach((el) => {
+		sum += parseFloat(el.value.replace(',', '.'));
+	});
+
+	outputSpan.textContent = `${sum.toFixed(2)} PLN`.replace('.', ',');
+	updateAllHiddenInputValues();
+};
+
+const updateAllHiddenInputValues = () => {
+	const totalGross = document.querySelector('.invoice-total-gross');
+	const totalGrossNum = parseAmountToCount(totalGross.textContent);
+
+	const totalNet = document.querySelector('.invoice-total-net');
+	const totalNetNum = parseAmountToCount(totalNet.textContent);
+
+	changeValueInHiddenInput(totalGrossNum, '#total-gross');
+	changeValueInHiddenInput(totalGrossNum, '#to-pay-numeric');
+	changeValueInHiddenInput(verbalNotation(totalGrossNum), '#to-pay-verbal');
+	changeValueInHiddenInput(totalNetNum, '#total-net');
+};
+
+document.addEventListener('DOMContentLoaded', updateServicesTable);
+
+// ============================================================
 // ADD NEW SERVICE TR IN TABLE
 const addNewServiceBtn = document.querySelector('.new-service-btn');
 
@@ -172,68 +237,3 @@ document.addEventListener('keydown', (e) => {
 		e.preventDefault();
 	}
 });
-
-// ============================================================
-// UPDATE DATA IN SERVICE TABLE
-const updateServicesTable = () => {
-	updateItemsNumbers();
-	showEmptyInfo();
-	calculateTableSummary();
-};
-
-const updateItemsNumbers = () => {
-	const itemsNoSpans = document.querySelectorAll(
-		'.invoice-edit__form-box-services-table .service-item-number'
-	);
-
-	const positionHiddenInputs = document.querySelectorAll(
-		'.position-hidden-input'
-	);
-
-	for (let i = 0; i < itemsNoSpans.length; i++) {
-		itemsNoSpans[i].textContent = i + 1;
-	}
-
-	for (let i = 0; i < positionHiddenInputs.length; i++) {
-		positionHiddenInputs[i].value = i + 1;
-	}
-};
-
-const showEmptyInfo = () => {
-	const services = document.querySelectorAll('#services-table tbody tr');
-	const emptyInfo = document.querySelector('#services-table tbody .empty-info');
-	emptyInfo.style.display = services.length === 1 ? 'grid' : 'none';
-};
-
-const calculateTableSummary = () => {
-	sumUpValues('.service-item-net-sum', '.invoice-total-net');
-	sumUpValues('.service-item-gross-sum', '.invoice-total-gross');
-};
-
-const sumUpValues = (inputsArrSelectorAll, outputSpanSelector) => {
-	const inputsArr = document.querySelectorAll(inputsArrSelectorAll);
-	const outputSpan = document.querySelector(outputSpanSelector);
-
-	let sum = 0;
-	inputsArr.forEach((el) => {
-		sum += parseFloat(el.value.replace(',', '.'));
-	});
-
-	outputSpan.textContent = `${sum.toFixed(2)} PLN`.replace('.', ',');
-	updateAllHiddenInputValues();
-};
-
-const updateAllHiddenInputValues = () => {
-	const totalGross = document.querySelector('.invoice-total-gross');
-	const totalGrossNum = parseAmountToCount(totalGross.textContent);
-
-	const totalNet = document.querySelector('.invoice-total-net');
-	const totalNetNum = parseAmountToCount(totalNet.textContent);
-
-	changeValueInHiddenInput(totalGrossNum, '#total-gross');
-	changeValueInHiddenInput(totalGrossNum, '#to-pay-numeric');
-	changeValueInHiddenInput(verbalNotation(totalGrossNum), '#to-pay-verbal');
-	changeValueInHiddenInput(totalNetNum, '#total-net');
-};
-
-document.addEventListener('DOMContentLoaded', updateServicesTable);
