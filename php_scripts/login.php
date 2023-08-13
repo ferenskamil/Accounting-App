e0ddf099c082dab1f06b299e22f15de0b7b7c5d3
Kamil <mr.kaam@gmail.com>
2023-08-13 10:34:11 +0200

    Feat: create an assoc array  (don't use yet) (login.php)

--- php_scripts/login.php
+++ php_scripts/login.php
@@ -10,7 +10,7 @@ require_once 'db_database.php';
 
 $user_query = $db->prepare("SELECT * FROM users
         WHERE login = :login");
-$user_query->bindValue(':login', $login , PDO::PARAM_STR);
+$user_query->bindValue(':login', $login, PDO::PARAM_STR);
 $user_query->execute();
 $users = $user_query->fetchAll(PDO::FETCH_ASSOC);
 
@@ -18,11 +18,33 @@ $users = $user_query->fetchAll(PDO::FETCH_ASSOC);
 if (count($users) === 1) {
         unset($_SESSION['log_error_login']);
 
-        // Index 0 is array with user data
-        $user = $users[0];
+        $user = [
+                // Flag whether user is logged in
+                'is_logged' => true,
+
+                // Basic information about account 
+                'id' => $users[0]['id'],
+                'login'=> $users[0]['login'],
+                'email'=> $users[0]['email'],
+
+                // Information about users's company. Will be insert to new invoice as default.
+                'company'=> $users[0]['company_name'],
+                'address1'=> $users[0]['address1'],
+                'address2'=> $users[0]['address2'],
+                'company_code'=> $users[0]['company_number'],
+                'city'=> $users[0]['default_invoice_city'],
+                'bank'=> $users[0]['default_invoice_bank_name'],
+                'account_no'=> $users[0]['default_invoice_bank_account_no'],
+                'additional_info'=> $users[0]['default_invoice_additional_info'],
+
+                // Information about the file names of a specific user 
+                'avatar' => $users[0]['avatar_file_img'],
+                'logo' => $users[0]['company_logo_file_path'],
+        ];
 
         // Check that the password entered by the user matches the password in the database 
-        if (password_verify($_POST['password1'], $user['password'])) {
+        $db_password = $users[0]['password'];
+        if (password_verify($_POST['password1'], $db_password)) {
                 unset($_SESSION['log_error_pass']);
 
                 $_SESSION['is_logged'] = true;
