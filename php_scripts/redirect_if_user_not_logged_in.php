9b072cb2c73b011de6700e336616680dab922ebb
Kamil <mr.kaam@gmail.com>
2023-08-13 11:01:57 +0200

    Feat: use  assoc array instead session variables (redirect_if_user_not_logged_in.php)

--- php_scripts/redirect_if_user_not_logged_in.php
+++ php_scripts/redirect_if_user_not_logged_in.php
@@ -1,7 +1,7 @@
 <?php 
 
 function redirect_if_user_not_logged_in ($file_directory) {
-        if (!isset($_SESSION['is_logged']) || !$_SESSION['is_logged']) {
+        if (!isset($_SESSION['user'])) {
                 header('Location: '.$file_directory);
                 exit();
         }
