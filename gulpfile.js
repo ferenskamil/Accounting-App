f5f0f62634db1bce9ca8a13b879703c9f969cef7
Kamil <mr.kaam@gmail.com>
2023-08-21 14:17:42 +0200

    Feat: update gulp ; delete unused packages and functions

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,7 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 const rename = require('gulp-rename');
 const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
-const php = require('gulp-connect-php');
-const reload = browserSync.reload;
 const clean = require('gulp-clean');
-
-const kit = require('gulp-kit');
 
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
@@ -19,30 +14,14 @@ const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 
 const paths = {
-	// html: './src/html/**/*.kit',
-	html: './src/php/**/*.kit',
 	sass: './src/scss/**/*.scss',
 	js: './src/js/**/*.js',
-	img: './src/img/**/*',
-	dist: './dist',
-	sassDest: './dist/css',
-	jsDest: './dist/js',
-	imgDest: './dist/img',
+	img: './src/app_img/**/*',
+	dist: './public',
+	sassDest: './assets/css',
+	jsDest: './assets/js',
+	imgDest: './assets/app_img',
 };
-
-function handleKits(done) {
-	src(paths.html)
-		.pipe(kit())
-		// This below will be using for change files enxtension to ".php"
-		.pipe(
-			rename(function (path) {
-				// Updates the object in-place  // path.dirname += '/ciao'; // path.basename += '-goodbye';
-				path.extname = '.php';
-			})
-		)
-		.pipe(dest('./'));
-	done();
-}
 
 function sassCompiler(done) {
 	src(paths.sass)
@@ -73,46 +52,13 @@ function convertImages(done) {
 	done();
 }
 
-function startBrowserSync(done) {
-	browserSync.init({
-		server: {
-			baseDir: './',
-		},
-	});
-	done();
-}
-
 function watchForChanges(done) {
-	// watch('./*.html').on('change', reload);
-	watch('./*.php').on('change', reload);
-	watch(
-		[paths.html, paths.sass, paths.js],
-		parallel(handleKits, sassCompiler, javaScript)
-	).on('change', reload);
+	watch([paths.sass, paths.js], parallel(sassCompiler, javaScript)).on(
+		'change',
+		reload
+	);
 	watch(paths.img, convertImages).on('change', reload);
 
-	done();
-}
-
-function watchPhp(done) {
-	watch(['./*.html', './*.php']).on('change', browserSync.reload);
-	done();
-}
-
-function sync(done) {
-	php.server({
-		base: './',
-		port: 3000,
-		keepalive: true,
-		// custom PHP locations
-		bin: '../../php/php.exe',
-		ini: '../../php/php.ini',
-	});
-	browserSync.init({
-		proxy: 'localhost:3000',
-		baseDir: './',
-		notify: false,
-	});
 	done();
 }
 
@@ -122,17 +68,12 @@ function cleanStuff(done) {
 }
 
 const mainFunctions = parallel(
-	handleKits,
 	sassCompiler,
 	javaScript,
 	convertImages
 );
 exports.default = series(
 	mainFunctions,
-	// startBrowserSync,
 	watchForChanges,
-	watchPhp,
-	sync
 );
 exports.cleanStuff = cleanStuff;
-exports.handleKits = handleKits;
