1bda687706752c950a7c6a49f52a9af35261840f
Kamil <mr.kaam@gmail.com>
2023-08-31 16:57:27 +0200

    Fix: fix validation of add_edit_form ; update other min.js files

--- src/js/pages/add_edit_form/edit_form_validation.js
+++ src/js/pages/add_edit_form/edit_form_validation.js
@@ -1,24 +1,39 @@
-// =============================================
-// EDIT FORM VALIDATION
-const inputs = document.querySelectorAll('.invoice-edit__form-box input');
-const submitBtn = document.querySelector(
-	'.invoice-edit__form-box-submit-btn button'
-);
+const addEditform = document.querySelector('.invoice-edit__form');
 
-const editFormValidation = (e) => {
-	let isOK = true;
+function addEditFormHandler(e, form) {
+	// block send form before validation
+	e.preventDefault();
+
+	// validation
+	if (emptyFieldsValidation(form)) {
+		// Send form
+		form.submit();
+	} else {
+		// Throw error
+		throw new Error('Cannot submit form. No field can be empty.');
+	}
+}
+
+function emptyFieldsValidation(form = null) {
+	const inputs = form.querySelectorAll('input');
+	const invalidInputs = [];
+
+	// Add css class to blank fields
 	inputs.forEach((input) => {
 		input.classList.remove('invoice-edit__form-box-input--empty');
 
 		if (input.value === '') {
 			input.classList.add('invoice-edit__form-box-input--empty');
-			isOK = false;
+
+			// Add invalid inputs to invalidInputs array
+			invalidInputs.push(input);
 		}
 	});
 
-	// Block submit form if any input is empty
-	// if (!isOK) e.preventDefault();
-	return isOK;
-};
+	// Return true or false depending on whether validation was successfull
+	return invalidInputs.length === 0 ? true : false;
+}
 
-submitBtn.addEventListener('click', (e) => editFormValidation(e));
+addEditform.addEventListener('submit', (e) =>
+	addEditFormHandler(e, addEditform)
+);
