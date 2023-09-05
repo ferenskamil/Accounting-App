c28facff4b0de22ecbbb8b8b89cdbdb85dfe0a0a
Kamil <mr.kaam@gmail.com>
2023-09-05 21:03:10 +0200

    Fix: use User class to connect to db in suggest_invoice_no.php

--- public/scripts/suggest_invoice_no.php
+++ public/scripts/suggest_invoice_no.php
@@ -1,5 +1,5 @@
 <?php
-// function to propose a new invoice number 
+// function to propose a new invoice number
 function suggest_invoice_no(int $num = 0) {
         $month = date('m');
         $year = date('Y');
@@ -8,60 +8,53 @@ function suggest_invoice_no(int $num = 0) {
         return "$num/$month/$year";
 };
 
-// Connect database
-require_once '../config/database/db_database.php';
-
 // Get user data to $user assoc array
 if (isset($_SESSION['user'])) $user = $_SESSION['user'];
 
-$db_invoices_nums = $db->prepare("
-        SELECT invoices.no FROM `invoices`
-        INNER JOIN users ON invoices.user_id = users.id
-        WHERE users.login = :login");
-$db_invoices_nums->bindvalue(':login', $user['login'], PDO::PARAM_STR);
-$db_invoices_nums->execute();
-$db_invoices_nums_arr = $db_invoices_nums->fetchAll(PDO::FETCH_ASSOC);
+// Get info about invoices from DB
+require_once '../class/user.class.php';
+$user_obj = new User();
+$invoices = $user_obj->get_all_invoices($user['id']);
+
+// Assign invoice numbers to $invoice_nums array
+$invoice_nums = [];
+foreach ($invoices as $invoice) $invoice_nums[] = $invoice['no'];
 
 // Sort the invoice numbers of a given user
-if (!empty($db_invoices_nums_arr)) {
-        foreach ($db_invoices_nums_arr as $invoice_row) {
-                $invoice_numbers[] = $invoice_row['no'];
-
+if (!empty($invoice_nums)) {
+        foreach($invoice_nums as $num) {
+                $invoice_nums_exploded[] = explode('/', $num);
         }
 
-        foreach($invoice_numbers as $num) {
-                $invoice_numbers_exploded[] = explode('/', $num);
-        }
-
-        usort($invoice_numbers_exploded, function($a, $b) {
+        usort($invoice_nums_exploded, function($a, $b) {
                 if ($b[2] !== $a[2]) {
                         return $b[2] - $a[2];
                 } else  if ($b[1] !== $a[1]) {
                         return $b[1] - $a[1];
                 } else {
                         return $b[0] - $a[0];
-                }                       
-        });       
+                }
+        });
 
-        $latest_invoice_no_num = $invoice_numbers_exploded[0][0];
-        $latest_invoice_no_month = $invoice_numbers_exploded[0][1];
-        $latest_invoice_no_year = $invoice_numbers_exploded[0][2];
+        $latest_invoice_no_num = $invoice_nums_exploded[0][0];
+        $latest_invoice_no_month = $invoice_nums_exploded[0][1];
+        $latest_invoice_no_year = $invoice_nums_exploded[0][2];
 }
 
-if ($db_invoices_nums->rowCount() === 0) {
+if (empty($invoice_nums)) {
         $_SESSION['suggestion_invoice_no'] = suggest_invoice_no();
 } else {
 
         if($latest_invoice_no_month >= date('m') && $latest_invoice_no_year >= date('Y')){
-                $_SESSION['suggestion_invoice_no'] = suggest_invoice_no($latest_invoice_no_num);    
-        } else {     
+                $_SESSION['suggestion_invoice_no'] = suggest_invoice_no($latest_invoice_no_num);
+        } else {
                 $_SESSION['suggestion_invoice_no'] = suggest_invoice_no();
         }
-        
+
         // validation - the number must be unique
-        while (in_array($_SESSION['suggestion_invoice_no'], $invoice_numbers )) {
+        while (in_array($_SESSION['suggestion_invoice_no'], $invoice_nums )) {
                 $validate_no = $_SESSION['suggestion_invoice_no'];
                 $validate_no = explode('/', $validate_no);
                 $_SESSION['suggestion_invoice_no'] = suggest_invoice_no($validate_no[0]);
-        } 
+        }
 }
