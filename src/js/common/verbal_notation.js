d6d24650cbce38e6552cccb040d156cb44ddaa6c
Kamil <mr.kaam@gmail.com>
2023-09-05 15:18:09 +0200

    Feat: passed all tests for verbal notation feature

--- src/js/common/verbal_notation.js
+++ src/js/common/verbal_notation.js
@@ -1,4 +1,5 @@
 export class VerbalNotation {
+	// class VerbalNotation {
 	constructor() {
 		this.unities = {
 			0: 'zero',
@@ -14,26 +15,26 @@ export class VerbalNotation {
 		};
 
 		this.dozens = {
-			2: 'twenty ',
-			3: 'thirty ',
-			4: 'forty ',
-			5: 'fifty ',
-			6: 'sixty ',
-			7: 'seventy ',
-			8: 'eighty ',
-			9: 'ninety ',
+			2: 'twenty',
+			3: 'thirty',
+			4: 'forty',
+			5: 'fifty',
+			6: 'sixty',
+			7: 'seventy',
+			8: 'eighty',
+			9: 'ninety',
 		};
 
 		this.hundreds = {
-			1: 'one hundred ',
-			2: 'two hundred ',
-			3: 'three hundred ',
-			4: 'four hundred ',
-			5: 'five hundred ',
-			6: 'six hundred ',
-			7: 'seven hundred ',
-			8: 'eight hundred ',
-			9: 'nine hundred ',
+			1: 'one hundred',
+			2: 'two hundred',
+			3: 'three hundred',
+			4: 'four hundred',
+			5: 'five hundred',
+			6: 'six hundred',
+			7: 'seven hundred',
+			8: 'eight hundred',
+			9: 'nine hundred',
 		};
 
 		this.aboveTen = {
@@ -49,7 +50,7 @@ export class VerbalNotation {
 			19: 'nineteen',
 		};
 
-		this.thousands_ends = {
+		this.thousandsVariety = {
 			0: 'thousand',
 			1: 'thousand',
 			2: 'thousand',
@@ -63,81 +64,214 @@ export class VerbalNotation {
 		};
 	}
 
-	verbalNotation(num) {
-		// num = num.toString();
-		// const threeDigitsPartsArr = [];
-		// const currency = '$';
-		// let rest = '00';
-		// let whole = (num - (num % 1)).toString();
-		// let workNum = whole;
+	toVerbalNotation(num) {
+		// initial validation
+		this.validateType(num);
 
-		// while (num[0] === '0') {
-		// 	num = num.substring(1);
-		// }
+		// sanitization
+		const cleanNumStr = this.sanitizeNumberString(num);
 
-		// if (num.includes('.')) {
-		// 	rest = num.substring(num.indexOf('.') + 1);
-		// 	rest = rest.concat('0');
-		// 	rest = rest.substring(0, 2);
-		// 	whole = num.substring(0, num.indexOf('.'));
-		// }
+		// extract value to a whole number and tenths
+		const whole = this.getWholeNumber(cleanNumStr);
+		const tenths = this.getTenths(cleanNumStr);
 
-		// while (workNum.length >= 3) {
-		// 	threeDigitsPartsArr.unshift(workNum.slice(-3));
-		// 	workNum = workNum.slice(0, -3);
-		// }
+		// Divide number to parts of 3 digits
+		let parts = this.divideStringToParts(whole, 3);
 
-		// if (workNum.length < 3 && workNum) {
-		// 	threeDigitsPartsArr.unshift(workNum);
-		// }
+		// zamień każdą część na notację słowną
+		parts.forEach((el) => {
+			el = this.verbalNotationOfThreeDigitNumber(parseInt(el));
+		});
 
-		// if (whole.length <= 3) {
-		// 	return `${this.verbalNotationOfThreeDigitNumber(
-		// 		threeDigitsPartsArr[0]
-		// 	)} ${rest}/100 ${currency}`;
-		// } else if (whole.length <= 6) {
-		// 	let thousandsForm = threeDigitsPartsArr[0].slice(-1);
+		// Połącz w 'verbalnotację dodając w opowiednich miejscach
+		const verbalNotation = this.concatPartsToVerbalNotation(parts);
 
-		// 	return `${this.verbalNotationOfThreeDigitNumber(
-		// 		threeDigitsPartsArr[0]
-		// 	)} ${
-		// 		this.thousands_ends[`${thousandsForm}`]
-		// 	} ${this.verbalNotationOfThreeDigitNumber(
-		// 		threeDigitsPartsArr[1]
-		// 	)} ${rest}/100 ${currency}`;
-		// }
+		// Create remineder (f.e. '10/100 $')
+		const remainder = `${tenths}/100 $`;
 
-		// return 'The number is too long';
+		// Return verbal notation of number
+		const result = verbalNotation + ' ' + remainder;
+		return result.replaceAll('  ', ' ');
+	}
+
+	validateType(num) {
+		if (isNaN(num)) throw new Error('wrong type of parameter (NaN)');
+		if (typeof num !== 'string' && typeof num !== 'number')
+			throw new Error(`wrong type of parameter (${typeof num})`);
+	}
+
+	sanitizeNumberString(num) {
+		// Convert number to string
+		if (typeof num === 'number') num = num.toString();
+
+		// Remain only digits, dots, commas
+		num = num.replace(/[^0-9.,]/g, '');
+
+		// Replace commas to dots
+		num = num.replaceAll(',', '.');
+
+		// Remove the second dot and every character after it
+		num = parseFloat(num).toString();
+
+		// Remove redundant zeros
+		while (
+			num.startsWith('0') &&
+			!num.startsWith('0.') &&
+			!num.startsWith('0,') &&
+			num !== '0'
+		) {
+			num = num.slice(1);
+		}
+
+		// Return sanitized num
+		return num;
+	}
+
+	getWholeNumber(num) {
+		return parseInt(num).toString();
+	}
+
+	getTenths(num) {
+		if (typeof num === 'string') num = parseFloat(num);
+		const roundedNum = this.roundDecimals(num);
+		const stringNum = roundedNum.toFixed(2);
+		const dotIdx = stringNum.indexOf('.');
+		const tenths = stringNum.substring(dotIdx + 1);
+		return tenths;
+	}
+
+	divideStringToParts(string, partLength) {
+		const resultArr = [];
+
+		while (string.length >= partLength) {
+			resultArr.unshift(string.slice(-partLength));
+			string = string.slice(0, -partLength);
+		}
+		if (string.length < partLength && string) {
+			resultArr.unshift(string);
+		}
+		return resultArr;
+	}
+
+	concatPartsToVerbalNotation(array) {
+		// Zamień na liczby
+		array.forEach((el) => (el = parseInt(el)));
+
+		// result
+		let result = '';
+
+		// Verbal notation for numbers 0 - 999
+		const mniejsze = array.pop();
+		const below1000 = this.verbalNotationOfThreeDigitNumber(mniejsze);
+
+		// Verbal notation for numbers 1000 - 999999
+		let above1000 = '';
+		if (array[0]) {
+			const wieksze = array.pop();
+			above1000 = this.verbalNotationOfThreeDigitNumber(wieksze);
+
+			// Variation of the word thousand
+			const thousandVariation = 'thousand';
+			above1000 += ' ' + thousandVariation + ' ';
+		}
+		return above1000 + below1000;
 	}
 
 	verbalNotationOfThreeDigitNumber(num) {
 		// Validation
 		if (num < 0 || num > 999 || num % 1 !== 0)
-			throw new Error('Invalid number.');
+			// throw new Error('Invalid number.');
 
-		// Variables
-		const numArr = num.split('');
-		const unit = numArr.pop();
-		const dozen = numArr.pop();
-		const hundred = numArr.pop();
+			// Variables
+			num = num.toString();
+		const numArr = num.toString().split('');
+		const unit = parseInt(numArr.pop());
+		const dozen = parseInt(numArr.pop());
+		const hundred = parseInt(numArr.pop());
 
 		// Stwórz zmienną, która będzie modyfikowana
 		let result = '';
 
 		// Jeżeli znaleziono liczbę setek
-		result += hundred ? this.hundreds[`${hundred}`] : '';
+		result += hundred ? this.hundreds[`${hundred}`] + ' ' : '';
 
 		// Jeżeli znaleziono liczbę dziesiątek i jest ona > 1
 		result +=
-			dozen > 1 ? this.dozens[`${dozen}`] + this.unities[`${unit}`] : '';
+			dozen > 1
+				? this.dozens[`${dozen}`] + ' ' + this.unities[`${unit}`]
+				: '';
 
 		// Jeżeli znaleziono liczbę dziesiątek i jest ona równa === 1
 		result += dozen === 1 ? this.aboveTen[`${dozen}${unit}`] : '';
 
 		// Jeżeli nie znalezino liczbę dziesiątek to dopisz liczbę jedności
-		result += dozen === 0 || !dozen ? this.unities[`${unit}`] : '';
+		if ((dozen === 0 || !dozen) && unit !== 0) {
+			result = this.unities[`${unit}`];
+		}
+
+		//
+		if (num === '0') result = this.unities[`${unit}`];
 
 		// Zwróć uzyskany wynik
+		result = result.replaceAll('  ', ' ');
 		return result;
 	}
+
+	getNumberOfDecimalDigits(num) {
+		// if (num % 1) {
+		// 	return 0;
+		// }
+
+		/// refactor nazw i testy i warunki brzegowe
+		num = num.toString();
+		const dotIdx = num.indexOf('.');
+		num = num.substring(dotIdx + 1);
+		return num.length;
+	}
+
+	roundDecimals(num) {
+		/// działa ale potrzebuje refaktoryzacji i testów
+		console.log(num);
+		// Zakładamy że liczba jest stringiem i ma rozwinięcie dziesiętne
+
+		const strNum = num.toString();
+		let numberOfDecimalDigits = this.getNumberOfDecimalDigits(num);
+		console.log(numberOfDecimalDigits);
+		if (numberOfDecimalDigits > 2) {
+			do {
+				const strNum = num.toString();
+				const lastDecimalDigit = strNum.at(-1);
+				const beforeRound = parseFloat(`0.${lastDecimalDigit}`);
+				const roundedLastDecimal = Math.round(beforeRound); //1
+
+				let numberTwoFromTheEnd = parseInt(strNum.at(-2));
+				console.log(numberTwoFromTheEnd);
+				if (roundedLastDecimal === 1) numberTwoFromTheEnd++;
+				console.log(numberTwoFromTheEnd);
+
+				// sklejanie
+				const roundedNumStr =
+					strNum.substring(0, strNum.length - 2) +
+					numberTwoFromTheEnd; //roundednumbertwofromtheend
+				console.log(roundedNumStr);
+				const roundedNum = parseFloat(roundedNumStr);
+				console.log(roundedNum + 'test');
+
+				// console.log(roundedLastDecimal);
+
+				// numberOfDecimalDigits = getNumberOfDecimalDigits(num);
+				num = roundedNum;
+
+				numberOfDecimalDigits = this.getNumberOfDecimalDigits(num);
+			} while (numberOfDecimalDigits > 2);
+		}
+
+		return num;
+	}
 }
+
+// const fn = new VerbalNotation();
+
+// // const result = fn.concatPartsToVerbalNotation(['10', '111']);
+// const result = fn.toVerbalNotation(100);
+// // const result2 = fn.verbalNotationOfThreeDigitNumber(999);
--- src/js/common/verbal_notation.test.js
+++ src/js/common/verbal_notation.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, test } from 'vitest';
-import { toVerbalNotation } from './verbal_notation.js';
+import { VerbalNotation } from './verbal_notation.js';
 
-const verb = new toVerbalNotation();
+const verb = new VerbalNotation();
 
 describe('toVerbalNotation()', () => {
 	describe('different lenght of number string range of 0 to 999999 ', () => {
@@ -57,43 +57,43 @@ describe('toVerbalNotation()', () => {
 		});
 	});
 
-	describe('Risky numbers', () => {
+	describe('Numbers >= 11 & <= 19', () => {
 		test.each([
 			{
 				num: 11,
-				expected: 'eleven 00/100$',
+				expected: 'eleven 00/100 $',
 			},
 			{
 				num: 12,
-				expected: 'twelve 00/100$',
+				expected: 'twelve 00/100 $',
 			},
 			{
 				num: 13,
-				expected: 'thirteen 00/100$',
+				expected: 'thirteen 00/100 $',
 			},
 			{
 				num: 14,
-				expected: 'fourteen 00/100$',
+				expected: 'fourteen 00/100 $',
 			},
 			{
 				num: 15,
-				expected: 'fifteen 00/100$',
+				expected: 'fifteen 00/100 $',
 			},
 			{
 				num: 16,
-				expected: 'sixteen 00/100$',
+				expected: 'sixteen 00/100 $',
 			},
 			{
 				num: 17,
-				expected: 'seventeen 00/100$',
+				expected: 'seventeen 00/100 $',
 			},
 			{
 				num: 18,
-				expected: 'eighteen 00/100$',
+				expected: 'eighteen 00/100 $',
 			},
 			{
-				num: 1,
-				expected: 'nineteen 00/100$',
+				num: 19,
+				expected: 'nineteen 00/100 $',
 			},
 		])(
 			'Should return the right verbal notation for number range of 11 to 19',
@@ -105,75 +105,44 @@ describe('toVerbalNotation()', () => {
 
 		test.each([
 			{
+				num: '0',
+				expected: 'zero 00/100 $',
+			},
+			{
 				num: '10',
-				expected: 'ten 00/100$',
+				expected: 'ten 00/100 $',
 			},
 			{
 				num: '100',
-				expected: 'one hundred 00/100$',
+				expected: 'one hundred 00/100 $',
 			},
 			{
 				num: '1000',
-				expected: 'one thousand 00/100$',
+				expected: 'one thousand 00/100 $',
 			},
 			{
 				num: '1100',
-				expected: 'one thousand one hundred 00/100$',
+				expected: 'one thousand one hundred 00/100 $',
 			},
 			{
 				num: '10000',
-				expected: 'ten thousand 00/100$',
+				expected: 'ten thousand 00/100 $',
 			},
 			{
 				num: '10100',
-				expected: 'ten thousand one hundred 00/100$',
+				expected: 'ten thousand one hundred 00/100 $',
 			},
 			{
 				num: '100000',
-				expected: 'one hundred thousand 00/100$',
+				expected: 'one hundred thousand 00/100 $',
 			},
 			{
 				num: '100100',
-				expected: 'one hundred thousand one hundred 00/100$',
+				expected: 'one hundred thousand one hundred 00/100 $',
 			},
 		])('Should return $expected for $num', ({ num, expected }) => {
 			const result = verb.toVerbalNotation(num);
 			expect(result).toBe(expected);
-		});
-	});
-
-	describe('Boundary values', () => {
-		test('Should throw error for number of 7 digits', () => {
-			const num1 = '1000000';
-			const num2 = 1000000;
-
-			const resultFn1 = () => verb.toVerbalNotation(num1);
-			const resultFn2 = () => verb.toVerbalNotation(num2);
-
-			expect(resultFn1).toThrow();
-			expect(resultFn2).toThrow();
-		});
-		test('Should pass zero as value', () => {
-			const num1 = '0';
-			const num2 = 0;
-
-			const result1 = verb.toVerbalNotation(num1);
-			const result2 = verb.toVerbalNotation(num2);
-
-			const expectedResult1 = 'zero 00/PLN $';
-			const expectedResult2 = 'zero 00/PLN $';
-			expect(result1).toBe(expectedResult1);
-			expect(result2).toBe(expectedResult2);
-		});
-		test('Should throw error for minus value', () => {
-			const num1 = '-1';
-			const num2 = -1;
-
-			const resultFn1 = () => verb.toVerbalNotation(num1);
-			const resultFn2 = () => verb.toVerbalNotation(num2);
-
-			expect(resultFn1).toThrow();
-			expect(resultFn2).toThrow();
 		});
 	});
 
@@ -212,47 +181,270 @@ describe('toVerbalNotation()', () => {
 			expect(result2).toBe(expectedResult2);
 		});
 	});
+});
 
-	describe('Sanitization of passed parameter', () => {
-		test('Should pass number as parameter', () => {
-			const num = 654894;
+describe('validateType()', () => {
+	test.each([
+		{
+			val: [],
+		},
+		{
+			val: {},
+		},
+		{
+			val: undefined,
+		},
+		{
+			val: NaN,
+		},
+		{
+			val: null,
+		},
+	])('should throw error for non transformable values ($val)', ({ val }) => {
+		const resultFn = () => verb.validateType(val);
 
-			const result = verb.toVerbalNotation(num);
+		expect(resultFn).toThrowError();
+	});
 
-			const expectedResult =
-				'six hundred fifty four thousand eight hundred ninety four 00/100 $';
-			expect(result).toBe(expectedResult);
-		});
-		test('Should remove zeros if they are at the beginning of the string', () => {
-			const input = '00005';
+	// test('Should throw error for negative value', () => {
+	// 	const num1 = '-1';
+	// 	const num2 = -1;
 
-			const result = verb.toVerbalNotation(input);
+	// 	const resultFn1 = () => verb.validateType(num1);
+	// 	const resultFn2 = () => verb.validateType(num2);
 
-			const expectedResult = 'five 00/100 $';
-			expect(result).toBe(expectedResult);
-		});
-		test('Should accept a number as a parameter if the digits are separated by spaces', () => {
-			const input = '1 000';
+	// 	expect(resultFn1).toThrow();
+	// 	expect(resultFn2).toThrow();
+	// });
+	// test('Should throw error for number of 7 digits', () => {
+	// 	const num1 = '1000000';
+	// 	const num2 = 1000000;
 
-			const result = verb.toVerbalNotation(input);
+	// 	const resultFn1 = () => verb.validateType(num1);
+	// 	const resultFn2 = () => verb.validateType(num2);
 
-			const expectedResult = 'one thousand 00/100 $';
-			expect(result).toBe(expectedResult);
-		});
-		test('Should accept a parameter if there are letters behind the numbers', () => {
-			const input = '1 USD';
+	// 	// expect('test').toBe(verb.validateType(num1))
+	// 	// expect('test').toBe(verb.validateType(num2))
 
-			const result = verb.toVerbalNotation(input);
+	// 	expect(resultFn1).toThrow();
+	// 	expect(resultFn2).toThrow();
+	// });
 
-			const expectedResult = 'one 00/100 $';
-			expect(result).toBe(expectedResult);
-		});
-		test('Should throw an error if the letters are between numbers', () => {
-			const input = '11s555';
+	test('should not throw error for string types', () => {
+		const input = '5';
 
-			const resultFn = () => verb.toVerbalNotation(input);
+		const resultFn = () => {
+			verb.validateType(input);
+		};
 
-			expect(resultFn).toThrow();
-		});
+		expect(resultFn).not.toThrow();
+	});
+
+	test('should not throw error for numbers', () => {
+		const input = 5;
+
+		const resultFn = () => {
+			verb.validateType(input);
+		};
+
+		expect(resultFn).not.toThrow();
 	});
 });
+
+describe('sanitizeNumberString()', () => {
+	test('Should yield string for number as parameter', () => {
+		const num = 5;
+
+		const result = verb.sanitizeNumberString(num);
+
+		expect(result).toBeTypeOf('string');
+	});
+	test('Should remove redundant zeros if they are at the beginning of the string', () => {
+		const input = '00005';
+
+		const result = verb.sanitizeNumberString(input);
+
+		const expectedResult = '5';
+		expect(result).toBe(expectedResult);
+	});
+	test('Should not remove the zero at the beginning if it is the only digit', () => {
+		const input = '0.1';
+
+		const result = verb.sanitizeNumberString(input);
+
+		const expectedResult = '0.1';
+		expect(result).toBe(expectedResult);
+	});
+
+	test('Should remove the second dot and all characters after it. ', () => {
+		const input = '55.6.85';
+
+		const result = verb.sanitizeNumberString(input);
+
+		const expectedResult = '55.6';
+		expect(result).toBe(expectedResult);
+	});
+
+	test('Should yield string number with dot for string number with comma', () => {
+		const input = '55,64';
+
+		const result = verb.sanitizeNumberString(input);
+
+		const expectedResult = '55.64';
+		expect(result).toBe(expectedResult);
+	});
+	test('Should not remove the zero at the beginning if it is followed by a comma or a dot, followed by a digit', () => {});
+
+	test.each([
+		{ input: '1 000', expected: '1000' },
+		{ input: '54 654', expected: '54654' },
+		{ input: ' 1 2 3 4 5 6', expected: '123456' },
+	])(
+		'Should read the number correctly, even if the digits are separated by spaces ("$input" => "$expected")',
+		({ input, expected }) => {
+			const result = verb.sanitizeNumberString(input);
+
+			expect(result).toBe(expected);
+		}
+	);
+	test('Should accept a parameter if there are letters behind the numbers', () => {
+		const input = '1 USD';
+
+		const result = verb.sanitizeNumberString(input);
+
+		const expectedResult = '1';
+		expect(result).toBe(expectedResult);
+	});
+	test('Should convert negative number for positive number', () => {
+		const input = '-5';
+
+		const result = verb.sanitizeNumberString(input);
+
+		const expectedResult = '5';
+		expect(result).toBe(expectedResult);
+	});
+});
+
+describe('getWholeNumber()', () => {
+	test('Should yield string with whole number for decimals', () => {
+		const input1 = '64.45';
+		const input2 = '64.4';
+
+		const result1 = verb.getWholeNumber(input1);
+		const result2 = verb.getWholeNumber(input2);
+
+		const expectedResult1 = '64';
+		const expectedResult2 = '64';
+		expect(result1).toBe(expectedResult1);
+		expect(result2).toBe(expectedResult2);
+	});
+
+	test('Should yield string type value', () => {
+		const input1 = '64';
+		const input2 = 64;
+
+		const result1 = verb.getWholeNumber(input1);
+		const result2 = verb.getWholeNumber(input2);
+
+		expect(result1).toBeTypeOf('string');
+		expect(result2).toBeTypeOf('string');
+	});
+	test.each([
+		{ input: '64', expected: '64' },
+		{ input: 64, expected: '64' },
+	])(
+		'Should return a string with an integer for an integer',
+		({ input, expected }) => {
+			const result = verb.getWholeNumber(input);
+			expect(result).toBe(expected);
+		}
+	);
+});
+
+describe('getTenths()', () => {
+	test('Should yield "00" for integer', () => {});
+	test('Should yield a two-digit remainder for a number with one digit after the decimal point ', () => {});
+	test('Should yield a two-digit remainder for a number with three digits after the decimal point ', () => {});
+	test.each([
+		{ val: '5.2', expected: '20' },
+		{ val: '5.24', expected: '24' },
+		{ val: '5.241', expected: '24' },
+	])(
+		'Should yield string with tenths of passed number',
+		({ val, expected }) => {
+			const result = verb.getTenths(val);
+
+			expect(result).toBe(expected);
+		}
+	);
+	test('Should round the number to two decimal places according to the rules of mathematics', () => {
+		const input = '5.427';
+
+		const result = verb.getTenths(input);
+
+		const expectedResult = '43';
+		expect(result).toBe(expectedResult);
+	});
+});
+
+describe('divideStringToParts()', () => {
+	test.each([
+		{ input: '12345678', partLength: 3, expected: ['12', '345', '678'] },
+		{ input: '1234567', partLength: 3, expected: ['1', '234', '567'] },
+	])(
+		'If the number of characters is indivisible by the parameter "partLength" then the element with index zero should have a smaller number of characters )',
+		({ input, partLength, expected }) => {
+			const result = verb.divideStringToParts(input, partLength);
+
+			expect(result).toStrictEqual(expected);
+		}
+	);
+	test('Should divide the string into elements of the given length and return an array with these elements', () => {
+		const input = '123456789';
+		const partLth = 3;
+
+		const result = verb.divideStringToParts(input, partLth);
+
+		const expectedResult = ['123', '456', '789'];
+		expect(result).toStrictEqual(expectedResult);
+	});
+	test('should return an array', () => {
+		const input = '1';
+		const partLth = 3;
+
+		// const testArray = new Array();
+		const result = verb.divideStringToParts(input, partLth);
+
+		expect(result).toBeInstanceOf(Array);
+	});
+	test('Any element in returned array should not be empty string', () => {
+		const input = 'abc';
+		const partLth = 3;
+
+		const result = verb.divideStringToParts(input, partLth);
+
+		const isAnyPartEmpty = result.includes('');
+		expect(isAnyPartEmpty).toBeFalsy();
+	});
+	test('Length of any array element should not be more than "partLength" parameter', () => {
+		const input = '111111111';
+		const partLth = 3;
+
+		const result = verb.divideStringToParts(input, partLth);
+
+		const maxPartLength = result.reduce(
+			(maxLength, el) => Math.max(maxLength, el.length),
+			0
+		);
+		expect(maxPartLength).toBeLessThanOrEqual(partLth);
+	});
+});
+
+describe.todo('verbalNotationOfThreeDigitNumber()', () => {});
+
+describe.todo('concatPartsToVerbalNotation()', () => {});
+
+describe.todo(
+	'zaokrąglijzgodnie za matematyką do danego miejsca po przecinku()',
+	() => {}
+);
